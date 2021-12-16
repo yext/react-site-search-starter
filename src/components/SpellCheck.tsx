3491851fe50fb1e9771724966c70e957673bc862
Yen Truong <ytruong@yext.com>
2021-12-16 14:59:51 -0500

    spellcheck tailwind

--- src/components/SpellCheck.tsx
+++ src/components/SpellCheck.tsx
@@ -1,26 +1,46 @@
 import { useAnswersState, useAnswersActions } from '@yext/answers-headless-react'
-import '../sass/SpellCheck.scss';
+import classNames from 'classnames';
+import { CompositionMethod, useComposedCssClasses } from '../hooks/useComposedCssClasses';
 
-interface Props {
-  isVertical: boolean
+interface SpellCheckCssClasses {
+  container?: string,
+  helpText?: string,
+  spellCheck___loading?: string,
+  spellCheckLink?: string
 }
 
-export default function SpellCheck (props: Props): JSX.Element | null {
+const builtInCssClasses: SpellCheckCssClasses = {
+  container: 'text-lg pt-3',
+  helpText: 'text-gray-600',
+  spellCheck___loading: 'opacity-50',
+  spellCheckLink: 'text-blue-600 font-bold cursor-pointer hover:underline focus:underline'
+}
+
+interface Props {
+  isVertical: boolean,
+  customCssClasses?: SpellCheckCssClasses,
+  cssCompositionMethod?: CompositionMethod
+}
+
+export default function SpellCheck ({ isVertical, customCssClasses, cssCompositionMethod }: Props): JSX.Element | null {
+  const cssClasses = useComposedCssClasses(builtInCssClasses, customCssClasses, cssCompositionMethod);
   const correctedQuery = useAnswersState(state => state.spellCheck.correctedQuery);
+  const isLoading = useAnswersState(state => state.searchStatus.isLoading);
+  const spellCheckClassNames = cssClasses.spellCheck___loading
+    ? classNames(cssClasses.container, { [cssClasses.spellCheck___loading]: isLoading })
+    : cssClasses.container;
   const answersActions = useAnswersActions();
   if (!correctedQuery) {
     return null;
   }
   return (
-    <div className='SpellCheck'>
-      <span className='SpellCheck__helpText'>Did you mean: </span>
-      <button className='SpellCheck__link' onClick={() => {
+    <div className={spellCheckClassNames}>
+      <span className={cssClasses.helpText}>Did you mean </span>
+      <button className={cssClasses.spellCheckLink} onClick={() => {
         answersActions.setQuery(correctedQuery);
-        answersActions.setSpellCheckEnabled(false);
-        props.isVertical
+        isVertical
           ? answersActions.executeVerticalQuery()
-          : answersActions.executeUniversalQuery() 
-        answersActions.setSpellCheckEnabled(true);
+          : answersActions.executeUniversalQuery();
       }}>{correctedQuery}</button>
     </div>
   );
