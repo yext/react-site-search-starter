fcb2c878057db7d8fbf6acf39f4990deecccbe4b
Oliver Shi <oshi@yext.com>
2021-12-17 15:58:43 -0500

    add tailwind styling for DirectAnswer
    Did not add feedback buttons, which are planned for a separate item.
    
    J=SLAP-1759
    TEST=manual
    
    check that both featured snippet and field value direct answers match the mocks
    used the us_presidents experience for featured snippet direct answers
    used mock link data, because we don't support using custom fields

--- src/components/DirectAnswer.tsx
+++ src/components/DirectAnswer.tsx
@@ -1,52 +1,99 @@
-import { useAnswersState, DirectAnswerType } from "@yext/answers-headless-react";
+import { useAnswersState, DirectAnswerType, DirectAnswer as DirectAnswerModel } from '@yext/answers-headless-react';
 import renderHighlightedValue from './utils/renderHighlightedValue';
-import classNames from "classnames";
-import '../sass/DirectAnswer.scss';
+import classNames from 'classnames';
+import { ReactNode } from 'react';
 
 interface DirectAnswerProps {
-  cssClasses?: {
-    container?: string,
-    containerLoading?: string,
-    title?: string,
-    content?: string,
-    description?: string
-  }
+  cssClasses?: DirectAnswerCssClasses
 }
 
-const defaultCSSClasses = {
-  container: 'DirectAnswer',
-  containerLoading: 'DirectAnswer--loading',
-  title: 'DirectAnswer__title',
-  content: 'DirectAnswer__content',
-  description: 'DirectAnswer__contentDescription'
+interface DirectAnswerCssClasses {
+  container?: string,
+  containerLoading?: string,
+  fieldValueTitle?: string,
+  featuredSnippetTitle?: string,
+  content?: string,
+  fieldValueDescription?: string,
+  featuredSnippetDescription?: string,
+  viewDetailsLink?: string,
+  viewDetailsLinkContainer?: string,
+  highlighted?: string
+}
+
+const defaultCSSClasses: DirectAnswerCssClasses = {
+  container: 'p-4 border border-color-zinc-200',
+  containerLoading: 'opacity-50',
+  fieldValueTitle: 'mb-4 text-gray-500',
+  featuredSnippetTitle: 'mb-4 font-bold text-xl',
+  content: '',
+  fieldValueDescription: 'font-bold text-xl',
+  featuredSnippetDescription: '',
+  viewDetailsLink: 'text-blue-600',
+  viewDetailsLinkContainer: 'pt-4',
+  highlighted: 'bg-blue-100'
 };
 
 export default function DirectAnswer(props: DirectAnswerProps): JSX.Element | null {
   const directAnswerResult = useAnswersState(state => state.directAnswer.result);
-  const isLoading = useAnswersState(state => state.searchStatus.isLoading);
+  console.log(directAnswerResult)
+  const isLoading = useAnswersState(state => state.searchStatus.isLoading || false);
   if (!directAnswerResult) {
     return null;
   }
-  const { cssClasses: customCssClasses } = props;
-  const cssClasses = Object.assign(defaultCSSClasses, customCssClasses);
-  const containerCssClasses = classNames(cssClasses.container, { [cssClasses.containerLoading]: isLoading });
+  const cssClasses = getCssClasses(props.cssClasses, isLoading, directAnswerResult);
+  const title = directAnswerResult.type === DirectAnswerType.FeaturedSnippet
+    ? directAnswerResult.value
+    : `${directAnswerResult.entityName} / ${directAnswerResult.fieldName}`
+  const description: ReactNode = directAnswerResult.type === DirectAnswerType.FeaturedSnippet 
+    ? renderHighlightedValue(directAnswerResult.snippet, { highlighted: cssClasses.highlighted })
+    : directAnswerResult.value;
+  const link = directAnswerResult.relatedResult.link;
+
+  function getLinkText(directAnswerResult: DirectAnswerModel) {
+    const isSnippet = directAnswerResult.type === DirectAnswerType.FeaturedSnippet;
+    const name = directAnswerResult.relatedResult.name;
+    if (isSnippet && name) {
+      return <>
+        Read more about <a className={cssClasses.viewDetailsLink} href={link}>
+          {directAnswerResult.relatedResult.name}
+        </a>
+      </>
+    } else if (!isSnippet && link) {
+      return <a href={link} className={cssClasses.viewDetailsLink}>View Details</a>
+    }
+  }
 
   return (
-    <div className={containerCssClasses}>
-      <div className={cssClasses.title}>
-        {directAnswerResult.type === DirectAnswerType.FeaturedSnippet
-          ? directAnswerResult.value
-          : `${directAnswerResult.entityName} / ${directAnswerResult.fieldName}`}
-      </div>
+    <div className={cssClasses.container}>
+      {title &&
+        <div className={cssClasses.title}>{title}</div>}
       <div className={cssClasses.content}>
-        <div className={cssClasses.description}>
-          {directAnswerResult.type === DirectAnswerType.FeaturedSnippet 
-            ? renderHighlightedValue(directAnswerResult.snippet)
-            : directAnswerResult.value}
-        </div>
-        {directAnswerResult.relatedResult.link 
-          && <a href={directAnswerResult.relatedResult.link}>View More</a>}
+        <div className={cssClasses.description}>{description}</div>
+        {link && 
+          <div className={cssClasses.viewDetailsLinkContainer}>
+            {getLinkText(directAnswerResult)}
+          </div>
+        }
       </div>
     </div>
   )
+}
+
+function getCssClasses(
+  customCssClasses: DirectAnswerCssClasses | undefined,
+  isLoading: boolean,
+  directAnswerResult: DirectAnswerModel
+) {
+  const cssClasses = Object.assign(defaultCSSClasses, customCssClasses);
+  const containerCssClasses = classNames(cssClasses.container, { [cssClasses.containerLoading || '']: isLoading });
+  const isSnippet = directAnswerResult.type === DirectAnswerType.FeaturedSnippet;
+  return {
+    container: containerCssClasses,
+    title: isSnippet ? cssClasses.featuredSnippetTitle : cssClasses.fieldValueTitle,
+    content: cssClasses.content,
+    description: isSnippet ? cssClasses.featuredSnippetDescription : cssClasses.fieldValueDescription,
+    viewDetailsLinkContainer: cssClasses.viewDetailsLinkContainer,
+    viewDetailsLink: cssClasses.viewDetailsLink,
+    highlighted: cssClasses.highlighted
+  }
 }
