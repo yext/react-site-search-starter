d3ca8ad6ecbc2fc403b6e4f626ba8edcdb82a166
nmanu1 <nmanu@yext.com>
2022-02-03 12:13:00 -0500

    Use inline styling

--- src/pagebuilder/models/AnswersAppConfig.ts
+++ src/pagebuilder/models/AnswersAppConfig.ts
@@ -2,16 +2,10 @@ import { HeadlessProviderConfig } from '../../config/answersHeadlessConfig';
 import { UniversalPageConfig } from './UniversalPageConfig';
 import { VerticalPageConfigs } from './VerticalPageConfig';
 import ComponentConfig from './ComponentConfig';
+import { StyleConfig } from './StyleConfig';
 
 interface CommonConfig extends ComponentConfig {
-  style: {
-    brandColor?: string,
-    baseFontSize?: string,
-    ctas?: {
-        borderRadius?: string,
-        color?: string
-    }
-  }
+  style: StyleConfig
 }
 
 export default interface AnswersAppConfig {
