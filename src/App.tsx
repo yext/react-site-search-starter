2fda4b4af8e0b6ce6fcbabda9acb8f64aadb51dd
nmanu1 <nmanu@yext.com>
2022-02-11 12:45:05 -0500

    Add Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,24 @@ import { AnswersHeadlessProvider } from '@yext/answers-headless-react';
 import { answersHeadlessConfig } from './config/answersHeadlessConfig';
 import { routeConfig } from './config/routeConfig';
 import { PageViewContextProvider } from './context/PageViewContext';
+import { AnalyticsProvider } from '@yext/answers-react-components';
+import { analyticsConfig } from './config/analyticsConfig';
 
 export default function App() {
   return (
     <AnswersHeadlessProvider {...answersHeadlessConfig}>
-      <PageViewContextProvider >
-        <div className='flex justify-center px-4 py-6'>
-          <div className='w-full max-w-5xl'>
-            <PageRouter
-              Layout={StandardLayout}
-              routes={routeConfig}
-            />
+      <AnalyticsProvider {...analyticsConfig}>
+        <PageViewContextProvider >
+          <div className='flex justify-center px-4 py-6'>
+            <div className='w-full max-w-5xl'>
+              <PageRouter
+                Layout={StandardLayout}
+                routes={routeConfig}
+              />
+            </div>
           </div>
-        </div>
-      </PageViewContextProvider>
+        </PageViewContextProvider>
+      </AnalyticsProvider>
     </AnswersHeadlessProvider>
   );
 }
---
+++ src/config/analyticsConfig.ts
@@ -0,0 +1,7 @@
+import { AnalyticsConfig } from '@yext/analytics';
+
+export const analyticsConfig: AnalyticsConfig = {
+  experienceKey: 'answers-js-docs',
+  experienceVersion: 'STAGING',
+  businessId: 3215760
+};
