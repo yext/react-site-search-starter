d17f1cb5a5f367cefed0c887e5996c3c5dcdfef4
Oliver Shi <oshi@yext.com>
2022-02-14 17:58:57 -0500

    add node modules components to tailwind content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './public/index.html',
+    './node_modules/@yext/answers-react-components/lib/**/*.js'
+  ],
   theme: {
     extend: {},
   },
